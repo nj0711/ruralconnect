8b527cfc247fd7ab0a3cebb82ae40d125aebf024
varsil suvagiya <varsilsuvagiya@gmail.com>
2025-11-08 17:06:48 +0530

    comment language translator

--- villages/bakrol/admin/header.php
+++ villages/bakrol/admin/header.php
@@ -244,8 +244,8 @@
                  </div>
                  <ul class="navbar-nav header-right">
                      <li class="nav-item dropdown header-profile">
-                         <div id="google_translate_element" class="translate-widget container mt-4 p-3 text-center">
-                         </div>
+                         <!-- <div id="google_translate_element" class="translate-widget container mt-4 p-3 text-center">
+                         </div> -->
                      </li>
 
 
