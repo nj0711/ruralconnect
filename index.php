fcb126cf502c9fd45fab7f72b3b20a32544a1734
varsil suvagiya <varsilsuvagiya@gmail.com>
2025-11-08 18:57:49 +0530

    carousel dynamic in user side home page

--- index.php
+++ index.php
@@ -205,89 +205,156 @@
 		</center>
 	</section>
 
+	<!--<div class="page-wrapper">
+	<section class="main-slider">
+		<div class="main-slider-swiper owl-carousel owl-theme">
+			<div class="item">
+				<div class="item-slider-bg" style="background-image: url(assets/image/bg/karamsad-bg.jpg);"></div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="slider-content">
+								<h1 class="section-title">Karamsad</h1>
+								<a href="villages/karamsad/index.php" class="btn btn-primary">Discover More</a>
+							</div>--><!-- slider-content -->
+	<!--	</div>--><!-- col-md-12 -->
+	<!--	</div>--><!-- row -->
+	<!--	</div>--><!-- container -->
+	<!--	</div>--><!--item-->
+	<!--	<div class="item">
+				<div class="item-slider-bg" style="background-image: url(assets/image/bg/tarapur.jpg);"></div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="slider-content">
+								<h1 class="section-title">Tarapur</h1>
+								<a href="villages/tarapur/index.php" class="btn btn-primary">Discover More</a>
+							</div>--><!-- slider-content -->
+	<!--	</div>--><!-- col-md-12 -->
+	<!--	</div>--><!-- row -->
+	<!--	</div>--><!-- container -->
+	<!--	</div>--><!--item-->
+	<!--</div>--><!-- main-slider-swiper -->
+	<!--</section>--><!--main-slider-->
+
+
 	<div class="page-wrapper">
 		<section class="main-slider">
 			<div class="main-slider-swiper owl-carousel owl-theme">
-				<div class="item">
-					<div class="item-slider-bg" style="background-image: url(assets/image/bg/vlg2.jpg);"></div>
-					<div class="container">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="slider-content">
-									<h1 class="section-title">Karamsad</h1>
-									<a href="villages/karamsad/index.php" class="btn btn-primary">Discover More</a>
-								</div><!-- slider-content -->
-							</div><!-- col-md-12 -->
-						</div><!-- row -->
-					</div><!-- container -->
-				</div><!--item-->
-				<div class="item">
-					<div class="item-slider-bg" style="background-image: url(assets/image/bg/vlg1.jpg);"></div>
-					<div class="container">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="slider-content">
-									<h1 class="section-title">Tarapur</h1>
-									<a href="villages/tarapur/index.php" class="btn btn-primary">Discover More</a>
-								</div><!-- slider-content -->
-							</div><!-- col-md-12 -->
-						</div><!-- row -->
-					</div><!-- container -->
-				</div><!--item-->
-			</div><!-- main-slider-swiper -->
-		</section><!--main-slider-->
 
-		<section class="about-section">
-			<div class="container">
-				<div class="row justify-content-between">
-					<div class="col-lg-5">
-						<div class="about-image">
-							<div class="about-image-inner ">
-								<img src="assets/image/gallery/sardar.jpeg" class="img-fluid" alt="img-2" style="padding-top:10px;">
-							</div><!--about-image-inner img-one-->
-							<div class="about-image-inner img-two">
-								<img src="assets/image/shapes/about-3.jpg" class="floated-image" alt="img-3">
-								<img src="assets/image/gallery/sardar2.jpg" class="img-fluid" alt="img-4">
-							</div><!--about-image-inner img-two-->
-						</div><!--about-image-->
-					</div><!--col-lg-6-->
-					<div class="col-lg-6">
-						<div class="about-inner">
-							<div class="section-title-box">
-								<div class="section-tagline">About</div><!-- section-tagline -->
-								<h5 class="section-title">Welcome to Village On Web</h5>
-								<p>In the age of digitalization, the concept of showcasing village information online may seem unconventional, but it is increasingly becoming a trend with the rise of websites dedicated to displaying details about various villages. This innovative approach not only provides a platform for villages to promote themselves to a global audience but also serves as a valuable resource for individuals looking to explore and learn more about different communities. Village On Web is at the forefront of this movement, offering a comprehensive database of village information accessible at your fingertips. Read on to discover how Village On Web is revolutionizing the way we interact with and learn about villages online.</p>
-							</div><!-- section-title-box -->
-						</div><!-- about-inner -->
-					</div><!--col-lg-6-->
-				</div><!-- row -->
-			</div><!-- container -->
-		</section><!--about-section-->
+				<?php
+				// Fetch the last 2 villages 
+				$query = "SELECT * FROM villages ORDER BY id DESC LIMIT 2";
+				$result = mysqli_query($conn, $query);
 
-		<section class="portfolio-section" style="margin-top:-200px;">
+				// Define the two default background images
+				$defaultBackgrounds = [
+					"assets/image/village_image/vlg_4.jpg",
+					"assets/image/village_image/vlgg.jpg"
+				];
+
+				if ($result && mysqli_num_rows($result) > 0) {
+					$i = 0;
+					while ($row = mysqli_fetch_assoc($result)) {
+						// Convert to sentence case
+						$villageName = ucfirst(strtolower($row['village_name']));
+						$folderName  = strtolower($row['village_name']);
+
+						// Check if the village has its own image uploaded
+						// (Make sure your villages table has a column named 'village_img')
+						$imagePath = !empty($row['village_img']) ? $row['village_img'] : '';
+
+						// Decide which background to use
+						if (!empty($imagePath) && file_exists($imagePath)) {
+							$bgImage = $imagePath;
+						} else {
+							$bgImage = $defaultBackgrounds[$i % 2]; // Use default one
+						}
+
+						$i++;
+				?>
+
+						<div class="item">
+							<div class="item-slider-bg" style="background-image: url('<?php echo htmlspecialchars($bgImage); ?>');"></div>
+							<div class="container">
+								<div class="row">
+									<div class="col-md-12">
+										<div class="slider-content text-center">
+											<h1 class="section-title"><?php echo htmlspecialchars($villageName); ?></h1>
+											<a href="villages/<?php echo htmlspecialchars($folderName); ?>/index.php"
+												class="btn btn-primary">Discover More</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+				<?php
+					}
+				} else {
+					echo "<p class='text-center'>No villages found.</p>";
+				}
+				?>
+
+			</div> <!-- main-slider-swiper -->
+		</section>
+	</div>
 
 
 
 
-			<section class="cta-five-section">
-				<div class="container">
-					<div class="cta-five-card">
-						<div class="cta-five-card-icon">
-							<i class="flaticon-file"></i>
-						</div><!-- cta-five-card-icon -->
-						<div class="cta-five-content">
-							<h4>Download List Of Villages In Anand</h4>
-							<p>There are many villages that can be found in Anand, but we may not be <br> able to list them all so you can check from this list.</p>
-						</div><!-- cta-five-content -->
-						<div class="cta-five-button">
-							<a href="https://drive.google.com/uc?export=download&id=10QmdF6aO-x1cWkyjZlEOTtsZLpTeRCI9" class="btn btn-primary">Download Files</a>
-						</div><!-- cta-five-button -->
-						<div class="cta-five-img">
-							<i class="flaticon-file"></i>
-						</div><!-- cta-five-img -->
-					</div><!--cta-five-card-->
-				</div><!-- container -->
-			</section><!--cta-five-section-->
+	<section class="about-section">
+		<div class="container">
+			<div class="row justify-content-between">
+				<div class="col-lg-5">
+					<div class="about-image">
+						<div class="about-image-inner ">
+							<img src="assets/image/gallery/sardar.jpeg" class="img-fluid" alt="img-2" style="padding-top:10px;">
+						</div><!--about-image-inner img-one-->
+						<div class="about-image-inner img-two">
+							<img src="assets/image/shapes/about-3.jpg" class="floated-image" alt="img-3">
+							<img src="assets/image/gallery/sardar2.jpg" class="img-fluid" alt="img-4">
+						</div><!--about-image-inner img-two-->
+					</div><!--about-image-->
+				</div><!--col-lg-6-->
+				<div class="col-lg-6">
+					<div class="about-inner">
+						<div class="section-title-box">
+							<div class="section-tagline">About</div><!-- section-tagline -->
+							<h5 class="section-title">Welcome to Village</h5>
+							<p>In the age of digitalization, the concept of showcasing village information online may seem unconventional, but it is increasingly becoming a trend with the rise of websites dedicated to displaying details about various villages. This innovative approach not only provides a platform for villages to promote themselves to a global audience but also serves as a valuable resource for individuals looking to explore and learn more about different communities. Village On Web is at the forefront of this movement, offering a comprehensive database of village information accessible at your fingertips. Read on to discover how Village On Web is revolutionizing the way we interact with and learn about villages online.</p>
+						</div><!-- section-title-box -->
+					</div><!-- about-inner -->
+				</div><!--col-lg-6-->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!--about-section-->
+
+
+
+	<!-- portfolio-content -->
+
+
+
+	<section class="cta-five-section">
+		<div class="container">
+			<div class="cta-five-card">
+				<div class="cta-five-card-icon">
+					<i class="flaticon-file"></i>
+				</div><!-- cta-five-card-icon -->
+				<div class="cta-five-content">
+					<h4>Download List Of Villages In Anand</h4>
+					<p>There are many villages that can be found in Anand, but we may not be <br> able to list them all so you can check from this list.</p>
+				</div><!-- cta-five-content -->
+				<div class="cta-five-button">
+					<a href="https://drive.google.com/uc?export=download&id=10QmdF6aO-x1cWkyjZlEOTtsZLpTeRCI9" class="btn btn-primary">Download Files</a>
+				</div><!-- cta-five-button -->
+				<div class="cta-five-img">
+					<i class="flaticon-file"></i>
+				</div><!-- cta-five-img -->
+			</div><!--cta-five-card-->
+		</div><!-- container -->
+	</section><!--cta-five-section-->
 	</div><!--page-wrapper-->
 
 	<!-- Footer Starts Here   --> <?php include "footer.php"; ?> <!-- Footer Ends Here -->
