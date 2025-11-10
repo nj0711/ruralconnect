f775e1478ce50427f091826a04f4f632567efb86
varsil suvagiya <varsilsuvagiya@gmail.com>
2025-11-10 17:04:25 +0530

    remove all images

--- villages/mehsana/admin/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="copyright">
-    <p>Copyright &copy; Designed &amp; Developed by <a href="https://www.spuvvn.edu/" target="_blank">Sardar Patel University</a> <?php echo date("Y"); ?></p>
-</div>
