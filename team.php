c8dddd0287cc90cb7d2d354530e8f1eb34f03977
varsil suvagiya <varsilsuvagiya@gmail.com>
2025-11-11 18:18:22 +0530

    update team.php

--- team.php
+++ team.php
@@ -26,7 +26,9 @@
 </head>
 
 <body>
-	<!--header--> <?php include "header.php"; ?> <!-- Header Ends Here -->
+	<!--header-->
+	<?php include "header.php"; ?>
+	<!-- Header Ends Here -->
 
 	<div class="page-wrapper">
 		<section class="page-banner">
@@ -35,13 +37,13 @@
 					<ul class="list-unstyled">
 						<li><a href="index.php">Home</a></li>
 						<li>Team</li>
-					</ul><!-- list-unstyled -->
-				</div><!-- page-breadcrumbs -->
+					</ul>
+				</div>
 				<div class="page-banner-title">
 					<h3>Team</h3>
-				</div><!-- page-banner-title -->
-			</div><!-- container -->
-		</section><!--page-banner-->
+				</div>
+			</div>
+		</section>
 
 		<section class="team-section">
 			<div class="container">
@@ -51,144 +53,184 @@
 							<div class="section-title-box">
 								<div class="section-tagline">team members</div>
 								<h2 class="section-title">Meet our Development Team</h2>
-							</div><!-- section-title-box -->
-						</div><!--team-inner-->
-					</div><!--col-12 col-md-12 col-lg-6 col-xl-6-->
+							</div>
+						</div>
+					</div>
 					<div class="col-12 col-md-12 col-lg-6 col-xl-6">
 						<div class="team-box">
 							<p>Meet the passionate team behind our village's web journey!</p>
-						</div><!--team-box-->
-					</div><!--col-12 col-md-12 col-lg-6 col-xl-6-->
-				</div><!-- row -->
+						</div>
+					</div>
+				</div>
+
 				<div class="row row-gutter-y-30">
+
+					<!-- 1. Sonal Jain (Project Head) -->
 					<div class="col-12 col-md-6 col-xl-4">
 						<div class="team-card">
 							<div class="team-card-img">
-								<img src="assets/image/team/sonal-mam.jpeg" class="img-fluid" alt="img-78">
-
-							</div><!-- team-card-img -->
+								<img src="assets/image/team/sonal-mam.jpeg" class="img-fluid" alt="Sonal S Jain">
+							</div>
 							<div class="team-card-content">
 								<h4><a href="">Sonal S Jain</a></h4>
 								<p><strong>Project Head</strong> <br />Post Graduate Department Of Computer Science And Technology, Anand</p>
-							</div><!-- team-card-content -->
-						</div><!--team-card-->
-					</div><!--col-12 col-md-6 col-xl-3-->
+							</div>
+						</div>
+					</div>
+
 					<div class="col-12 col-md-6 col-xl-4">
 						<div class="team-card">
 							<div class="team-card-img">
-								<img src="assets/image/team/sudhir-rajai.jpeg" class="img-fluid" alt="img-79">
+								<img src="assets/image/team/shrutipic_team.jpg" class="img-fluid" alt="Shruti Gohil">
+								<div class="team-card-icon">
+									<a href="#" class="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
+									<a href="#" class="twitter"><i class="fa-brands fa-instagram"></i></a>
+									<a href="https://linkedin.com/in/Shruti-Gohil" class="facebook"><i class="fa-brands fa-linkedin"></i></a>
+								</div>
+							</div>
+							<div class="team-card-content">
+								<h4><a href="https://linkedin.com/in/Shruti-Gohil">Shruti Gohil</a></h4>
+								<p>Post Graduate Student, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
+
+							</div>
+						</div>
+					</div>
+
+
+					<div class="col-12 col-md-6 col-xl-4">
+						<div class="team-card">
+							<div class="team-card-img">
+								<img src="assets/image/team/varsil.jpg" class="img-fluid" alt="Varsil Suvagiya">
+								<div class="team-card-icon">
+									<a href="#" class="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
+									<a href="https://instagram.com/" class="twitter"><i class="fa-brands fa-instagram"></i></a>
+									<a href="https://linkedin.com/in/" class="facebook"><i class="fa-brands fa-linkedin"></i></a>
+								</div>
+							</div>
+							<div class="team-card-content">
+								<h4><a href="https://www.linkedin.com/in/varsil-suvagiya-548aba253/?isSelfProfile=true">Varsil Suvagiya</a></h4>
+
+								<p>Post Graduate Student, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand
+							</div>
+						</div>
+					</div>
+
+					<!-- Remaining Original Members -->
+					<div class="col-12 col-md-6 col-xl-4">
+						<div class="team-card">
+							<div class="team-card-img">
+								<img src="assets/image/team/sudhir-rajai.jpeg" class="img-fluid" alt="Sudhir Rajai">
 								<div class="team-card-icon">
 									<a href="#" class="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
 									<a href="#" class="twitter"><i class="fa-brands fa-instagram"></i></a>
 									<a href="https://in.linkedin.com/in/rajai-sudhir-52796a214" class="facebook"><i class="fa-brands fa-linkedin"></i></a>
-								</div><!-- team-card-icon -->
-							</div><!-- team-card-img -->
+								</div>
+							</div>
 							<div class="team-card-content">
 								<h4><a href="https://in.linkedin.com/in/rajai-sudhir-52796a214">Sudhir N Rajai</a></h4>
-								<p>Post Graduate Sudent, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
-							</div><!-- team-card-content -->
-						</div><!--team-card-->
-					</div><!--col-12 col-md-6 col-xl-3-->
+								<p>Post Graduate Student, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
+							</div>
+						</div>
+					</div>
+
 					<div class="col-12 col-md-6 col-xl-4">
 						<div class="team-card">
 							<div class="team-card-img">
-								<img src="assets/image/team/jagdish-rajai.jpeg" class="img-fluid" alt="img-80">
+								<img src="assets/image/team/jagdish-rajai.jpeg" class="img-fluid" alt="Jagdish Rajai">
 								<div class="team-card-icon">
 									<a href="#" class="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
 									<a href="#" class="twitter"><i class="fa-brands fa-instagram"></i></a>
 									<a href="https://www.linkedin.com/in/jagdish-rajai" class="facebook"><i class="fa-brands fa-linkedin"></i></a>
-								</div><!-- team-card-icon -->
-							</div><!-- team-card-img -->
+								</div>
+							</div>
 							<div class="team-card-content">
 								<h4><a href="https://www.linkedin.com/in/jagdish-rajai">Jagdish K Rajai</a></h4>
-								<p>Post Graduate Sudent, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
-							</div><!-- team-card-content -->
-						</div><!--team-card-->
-					</div><!--col-12 col-md-6 col-xl-3-->
+								<p>Post Graduate Student, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
+							</div>
+						</div>
+					</div>
 
-				</div><!-- row -->
-
-
-				<div class="row row-gutter-y-30 mt-5">
-
-					<div class="col-12 col-md-6 col-xl-3">
+					<div class="col-12 col-md-6 col-xl-4">
 						<div class="team-card">
 							<div class="team-card-img">
-								<img src="assets/image/team/nishtha-dholakiya.jpeg" class="img-fluid" alt="img-78">
+								<img src="assets/image/team/nishtha-dholakiya.jpeg" class="img-fluid" alt="Nishtha Dholakiya">
 								<div class="team-card-icon">
 									<a href="#" class="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
 									<a href="#" class="twitter"><i class="fa-brands fa-instagram"></i></a>
 									<a href="https://www.linkedin.com/in/nishthadholakiya" class="facebook"><i class="fa-brands fa-linkedin"></i></a>
-								</div><!-- team-card-icon -->
-							</div><!-- team-card-img -->
+								</div>
+							</div>
 							<div class="team-card-content">
 								<h4><a href="https://www.linkedin.com/in/nishthadholakiya">Nishtha A Dholakiya</a></h4>
-								<p>Post Graduate Sudent, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
-							</div><!-- team-card-content -->
-						</div><!--team-card-->
-					</div><!--col-12 col-md-6 col-xl-3-->
-					<div class="col-12 col-md-6 col-xl-3">
+								<p>Post Graduate Student, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 col-xl-4">
 						<div class="team-card">
 							<div class="team-card-img">
-								<img src="assets/image/team/nikita-darji.jpeg" class="img-fluid" alt="img-79">
+								<img src="assets/image/team/nikita-darji.jpeg" class="img-fluid" alt="Nikita Darji">
 								<div class="team-card-icon">
 									<a href="#" class="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
 									<a href="#" class="twitter"><i class="fa-brands fa-instagram"></i></a>
 									<a href="#" class="facebook"><i class="fa-brands fa-linkedin"></i></a>
-								</div><!-- team-card-icon -->
-							</div><!-- team-card-img -->
+								</div>
+							</div>
 							<div class="team-card-content">
 								<h4><a href="">Nikita C Darji</a></h4>
-								<p>Post Graduate Sudent, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
-							</div><!-- team-card-content -->
-						</div><!--team-card-->
-					</div><!--col-12 col-md-6 col-xl-3-->
-					<div class="col-12 col-md-6 col-xl-3">
+								<p>Post Graduate Student, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 col-xl-4">
 						<div class="team-card">
 							<div class="team-card-img">
-								<img src="assets/image/team/team-4.jpg" class="img-fluid" alt="img-81">
+								<img src="assets/image/team/dummy.jpg" class="img-fluid" alt="Priyank Rajai">
 								<div class="team-card-icon">
 									<a href="#" class="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
 									<a href="#" class="twitter"><i class="fa-brands fa-instagram"></i></a>
 									<a href="#" class="facebook"><i class="fa-brands fa-linkedin"></i></a>
-								</div><!-- team-card-icon -->
-							</div><!-- team-card-img -->
+								</div>
+							</div>
 							<div class="team-card-content">
 								<h4><a href="">Priyank S Rajai</a></h4>
-								<p>Post Graduate Sudent, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
-							</div><!-- team-card-content -->
-						</div><!--team-card-->
-					</div><!--col-12 col-md-6 col-xl-3-->
-					<div class="col-12 col-md-6 col-xl-3">
+								<p>Post Graduate Student, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 col-xl-4">
 						<div class="team-card">
 							<div class="team-card-img">
-								<img src="assets/image/team/team-3.jpg" class="img-fluid" alt="img-80">
+								<img src="assets/image/team/dummy.jpg" class="img-fluid" alt="Riya Panchal">
 								<div class="team-card-icon">
 									<a href="#" class="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
 									<a href="#" class="twitter"><i class="fa-brands fa-instagram"></i></a>
 									<a href="#" class="facebook"><i class="fa-brands fa-linkedin"></i></a>
-								</div><!-- team-card-icon -->
-							</div><!-- team-card-img -->
+								</div>
+							</div>
 							<div class="team-card-content">
 								<h4><a href="">Riya M Panchal</a></h4>
-								<p>Post Graduate Sudent, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
-							</div><!-- team-card-content -->
-						</div><!--team-card-->
-					</div><!--col-12 col-md-6 col-xl-3-->
+								<p>Post Graduate Student, MCA<br />Post Graduate Department Of Computer Science And Technology, Anand</p>
+							</div>
+						</div>
+					</div>
 
-				</div><!-- row -->
-			</div><!-- container -->
-		</section><!--team-section-->
+				</div>
+			</div>
+		</section>
+	</div>
 
-	</div><!--page-wrapper-->
+	<!-- Footer Starts Here -->
+	<?php include "footer.php"; ?>
+	<!-- Footer Ends Here -->
 
+	<a href="#" class="scroll-to-top scroll-to-target" data-target="html">
+		<i class="fa-solid fa-arrow-up"></i>
+	</a>
 
-	<!-- Footer Starts Here   --> <?php include "footer.php"; ?> <!-- Footer Ends Here -->
-
-
-
-
-	<a href="#" class="scroll-to-top scroll-to-target" data-target="html"><i class="fa-solid fa-arrow-up"></i></a>
 	<script src="assets/vendor/bootstrap/bootstrap.bundle.min.js"></script>
 	<script src="assets/vendor/jquery/jquery.min.js"></script>
 	<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
